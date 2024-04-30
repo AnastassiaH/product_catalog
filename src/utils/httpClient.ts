482a87cb53513f2e336ba629d13017e6835913a0
Anastasiia Hurska <nastyagurska@gmail.com>
2024-04-30 22:21:17 +0300

    fixed

--- src/utils/httpClient.ts
+++ src/utils/httpClient.ts
@@ -1,7 +1,9 @@
 const BASE_URL = "https://github.com/AnastassiaH/product_catalog/tree/main/public/api";
 
 export function getData<T>(url: string): Promise<T> {
-  return fetch(BASE_URL + url).then((response) => {
+  return fetch(BASE_URL + url, {
+    mode: 'no-cors'
+  }).then((response) => {
     if (!response.ok) {
       throw new Error(`${response.status} ${response.text}`);
     }
