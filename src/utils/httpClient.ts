3a27203d9e602a3ed360861c31b0bb040457f781
Anastasiia Hurska <nastyagurska@gmail.com>
2024-05-02 02:27:07 +0300

    api url fixed

--- src/utils/httpClient.ts
+++ src/utils/httpClient.ts
@@ -1,10 +1,30 @@
-const BASE_URL = "https://github.com/AnastassiaH/product_catalog/public/api";
+// const BASE_URL = "https://github.com/AnastassiaH/product_catalog/public/api";
 
-export function getData<T>(url: string): Promise<T> {
-  return fetch(BASE_URL + url).then((response) => {
-    if (!response.ok) {
-      throw new Error(`${response.status} ${response.text}`);
-    }
-    return response.json();
-  });
+// export function getData<T>(url: string): Promise<T> {
+//   return fetch(BASE_URL + url).then((response) => {
+//     if (!response.ok) {
+//       throw new Error(`${response.status} ${response.text}`);
+//     }
+//     return response.json();
+//   });
+// }
+
+const URL = "https://anastassiah.github.io/product_catalog";
+
+const API_ENDPOINT = `${URL}/api/`;
+
+export async function getData<T>(url: string): Promise<T | null> {
+  const response = await fetch(`${API_ENDPOINT}${url}`);
+
+  if (!response || !response.ok) {
+    throw new Error(`Error occurred: ${response.statusText}`);
+  }
+
+  const data = await response.json();
+
+  if (!data) {
+    return null;
+  }
+
+  return data;
 }
