f93c47b8cb09a1a9fab8f0e5bbe6874d136ee3f6
Anastasiia Hurska <nastyagurska@gmail.com>
2024-05-03 19:23:33 +0300

    temp

--- src/utils/httpClient.ts
+++ src/utils/httpClient.ts
@@ -1,9 +1,10 @@
  const BASE_URL = "https://github.com/AnastassiaH/product_catalog";
 
-const API_ENDPOINT = `${BASE_URL}/api`;
+const API_ENDPOINT = `${BASE_URL}/public/api`;
+const PROXY_URL = 'https://cors-anywhere.herokuapp.com/'
 
 export function getData<T>(url: string): Promise<T> {
-  return fetch('http://localhost:3000/api' + url).then((response) => {
+  return fetch(PROXY_URL + API_ENDPOINT + url).then((response) => {
     if (!response.ok) {
       throw new Error(`${response.status} ${response.text}`);
     }
