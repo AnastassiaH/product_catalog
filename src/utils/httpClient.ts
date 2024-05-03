ba61b29d8b3a3b219c42e57fe62852889b842dea
Anastasiia Hurska <nastyagurska@gmail.com>
2024-05-03 19:32:38 +0300

    temp

--- src/utils/httpClient.ts
+++ src/utils/httpClient.ts
@@ -1,7 +1,7 @@
  const BASE_URL = "https://github.com/AnastassiaH/product_catalog";
 
 const API_ENDPOINT = `${BASE_URL}/public/api`;
-const PROXY_URL = 'https://cors-anywhere.herokuapp.com/'
+const PROXY_URL = 'https://thingproxy.freeboard.io/fetch/'
 
 export function getData<T>(url: string): Promise<T> {
   return fetch(PROXY_URL + API_ENDPOINT + url).then((response) => {
