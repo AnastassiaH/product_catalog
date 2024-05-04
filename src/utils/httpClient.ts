84d2995a3d331a0ddec53f6125af49f40c681c4c
Anastasiia Hurska <nastyagurska@gmail.com>
2024-05-04 12:28:07 +0300

    fixed

--- src/utils/httpClient.ts
+++ src/utils/httpClient.ts
@@ -1,10 +1,9 @@
  const BASE_URL = "https://github.com/AnastassiaH/product_catalog";
 
-const API_ENDPOINT = `${BASE_URL}/public/api`;
-const PROXY_URL = 'https://thingproxy.freeboard.io/fetch/'
+const API_ENDPOINT = `api`;
 
 export function getData<T>(url: string): Promise<T> {
-  return fetch(PROXY_URL + API_ENDPOINT + url).then((response) => {
+  return fetch(API_ENDPOINT + url).then((response) => {
     if (!response.ok) {
       throw new Error(`${response.status} ${response.text}`);
     }
